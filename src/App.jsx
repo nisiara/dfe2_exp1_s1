8e7d27f2b06bcaa4de83fbad250d8aa1916fcd7b
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-10-21 20:26:26 -0300

    Add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <main>
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+      
     </main>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,14 @@
-const Header = () => 'Este es el header'
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  )
+}
  
 export default Header;
